4bb17e91818e0ca7fb1fcc08b0b1f938ee4686fe
xipper <864353360@qq.com>
2022-12-25 15:44:44 +0800

    use iris

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"github.com/kataras/iris/v12"
+)
+
 func main() {
-	
+	app := iris.New()
+	app.Logger().SetLevel("debug")
+	template := iris.HTML("./backend/web/views", ".html").Layout("shared/layout.html").Reload(true)
+	app.RegisterView(template)
+	app.HandleDir("/assets", "./backend/web/assets")
+	app.OnAnyErrorCode(func(context iris.Context) {
+		context.ViewData("message", context.Values().GetStringDefault("message", "Page Error"))
+		context.ViewLayout("")
+		context.View("shared/error.html")
+	})
+
+	app.Run(
+		iris.Addr("localhost:9001"),
+		iris.WithoutServerError(iris.ErrServerClosed),
+		iris.WithOptimizations,
+	)
 }
